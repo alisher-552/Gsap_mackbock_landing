49bc69c6551e159e34a9d3f590f1696a75f0a9db
alisher-552 <alisherburbaev552@gmail.com>
2026-06-13 22:25:49 +0500

    Добавил 3D ноутбук , но кнопки пока не работают для переключения размера и цвета

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Hero from './components/Hero'
 import NavBar from './components/navBar'
+import ProductViewer from './components/ProductViewer'
 
 function App() {
   return (
     <main>
       <NavBar />
       <Hero />
+      <ProductViewer />
     </main>
   )
 }
